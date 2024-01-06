0380700c18026124da40b6ada1fb527a074ff376
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2024-01-06 06:45:25 +0300

    Add FolderPage with drag&drop upload support

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import DrivePanels from 'components/drive/layout/DrivePanels.jsx';
+import FolderPage from 'components/drive/main/FolderPage.jsx';
 
 import LoginPage from 'components/auth/LoginPage.jsx';
 import SignupPage from 'components/auth/SignupPage.jsx';
 
 export default function App () {
-
   return (
     <BrowserRouter>
       <Routes>   
-        <Route path='/' element={<Navigate replace to='/drive' />} />
+        <Route path='/' element={<Navigate replace to='/drive/home' />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
-        
+        <Route exact path='drive' element={<Navigate replace to='/drive/home' />} />
+        <Route path='/drive' element={<DrivePanels />}>
+          <Route path='folder/:id' element={<FolderPage />} />
+          <Route path='home' element={<FolderPage />} />
+          <Route path='trash' element={<FolderPage />} />
+        </Route>
       </Routes> 
     </BrowserRouter>    
   );
---
+++ src/components/drive/main/FolderPage.jsx
@@ -0,0 +1,97 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import axios from 'axios';
+
+export default function FolderPage () {
+  const userData = JSON.parse(sessionStorage.getItem('user'));
+
+  const [file, setFile] = useState();
+  const [fileUuid, setFileUuid] = useState();
+
+  const [requiresUpload, setRequiresUpload] = useState();
+  const [progress, setProgress] = useState(0);
+
+  const [children, setChildren] = useState();
+
+  const onDrop = useCallback(acceptedFiles => {
+    console.log(acceptedFiles);
+    handleChange(acceptedFiles[0])
+  }, []);
+
+  const {
+    getRootProps,
+    getInputProps
+  } = useDropzone({
+    onDrop, multiple: false, noDragEventsBubbling: true, //noClick: true
+  });
+
+
+  const handleChange = (file) => {
+    setFile(file);
+    if (file.size < (1024 * 1024 * 100)) {
+      setRequiresUpload(true);
+    } else {
+      setProgress(0);
+    }
+  };
+
+  useEffect(() => {
+    if (requiresUpload)
+    {
+      handleSubmit();
+      setRequiresUpload(false);
+    }
+  });
+
+
+  const handleSubmit = async () => {
+    const headers = { 'Authorization': `Bearer ${userData.accessToken}` };
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    formData.append('body', JSON.stringify({ ownerUuid: userData.userUuid, parentUuid: '/root', driveUuid: userData.driveUuid }));
+    
+    await axios.post(process.env.REACT_APP_BACKEND_URL + '/file/upload', formData, {headers}, {
+      onUploadProgress: (progressEvent) => {
+        const progress = (progressEvent.loaded / progressEvent.total);
+        setProgress(progress);
+      }})
+      .then(res => {
+        setFileUuid(res.data.fileUuid)
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
+
+  useEffect(() => {
+    const headers = { 'Authorization': `Bearer ${userData.accessToken}` };
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    formData.append('body', JSON.stringify({ ownerUuid: userData.userUuid, parentUuid: '/root', driveUuid: userData.driveUuid }));
+    
+    axios.post(process.env.REACT_APP_BACKEND_URL + '/file/upload', formData, {headers}, {
+      onUploadProgress: (progressEvent) => {
+        const progress = (progressEvent.loaded / progressEvent.total);
+        setProgress(progress);
+      }})
+      .then(res => {
+        setFileUuid(res.data.fileUuid)
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  })
+
+
+  return (
+    <div className='w-full h-full
+      bg-neutral-600' {...getRootProps()}>
+      <input {...getInputProps()} />
+    </div>
+  );
+}
