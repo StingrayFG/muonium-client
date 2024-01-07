4aa41c0bf4840ce55847000d3051e429650212dc
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2024-01-07 11:15:11 +0300

    Add file icon placeholder

--- src/components/drive/main/element/FileElement.jsx
+++ src/components/drive/main/element/FileElement.jsx
@@ -3,9 +3,15 @@ import React from "react";
 export default function FileElement ({ file, handleContextMenuClick }) {
   return (
     <div className='w-full h-full grid place-self-center
-    border-solid border-2 border-black' onContextMenu={(event) => {handleContextMenuClick(event, file)}}>
-      <div className='w-48 h-48 border-solid border-2 border-white rounded-tr-3xl place-self-center'>
-        FILE
+    border-solid border-0 border-black'>
+      <div className='w-48 h-48 place-self-center grid
+      border-solid border-0 border-black rounded-lg' 
+      onContextMenu={(event) => {handleContextMenuClick(event, file)}}>
+        <div className='w-36 h-48 place-self-center
+        bg-gradient-to-b from-neutral-300 to-neutral-400
+        border-solid border-2 border-neutral-400 rounded-lg'>
+        </div>
+
       </div>
       <p className='place-self-center text-center'>
         {file.name}
