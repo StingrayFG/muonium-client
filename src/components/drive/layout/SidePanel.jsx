c1686a7f214addc8a6dc73e1c827b46cb4a811c6
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2024-01-07 11:19:30 +0300

    Add icons to top and side panels

--- src/components/drive/layout/SidePanel.jsx
+++ src/components/drive/layout/SidePanel.jsx
@@ -4,8 +4,8 @@ export default function SidePanel () {
   return (
     <div className='w-96 h-full
       text-2xl text-neutral-200
-      bg-neutral-600
-      border-solid border-r-2 border-black'>
+      bg-gradient-to-b from-zinc-600 to-zinc-600 
+      border-solid border-r-2 border-zinc-800'>
 
         <Link className='w-[22rem] ml-4 mt-4 px-2 py-2 flex 
         hover:bg-cyan-900 active:bg-cyan-800'
--- src/components/drive/layout/TopPanel.jsx
+++ src/components/drive/layout/TopPanel.jsx
@@ -15,28 +15,45 @@ export default function TopPanel () {
   };
 
   return (
-    <div className='w-full h-24 px-4 py-4 flex
-      bg-gradient-to-b from-neutral-600 to-neutral-700
-      border-solid border-b-2 border-black'>
-        <button className='w-16 h-16
-        bg-white'>
-          Prev
-        </button>
-        <button className='w-16 h-16 ml-4
-        bg-white'>
-          Next
-        </button>
-        <div className='w-[50rem] h-16 ml-64 bg-white'>
-          <p className='mt-5 ml-5'>Route</p>
+    <div className='w-full h-20 px-4 py-4 flex
+      bg-gradient-to-b from-zinc-600 to-zinc-700
+      border-solid border-b-2 border-zinc-800
+      text-lg font-semibold font-sans text-neutral-200'>
+        <div className='flex'>
+          <button className='w-12 h-12 grid
+          bg-gradient-to-b from-neutral-300 to-neutral-400 
+          border-solid border-2 border-neutral-400 rounded-l-lg'>
+            <img src='/icons/chevron-left.svg' alt='prev' width='28' className='place-self-center'/>
+          </button>
+          <button className='w-12 h-12 grid
+          bg-gradient-to-b from-neutral-300 to-neutral-400 
+          border-solid border-2 border-neutral-400 rounded-r-lg'>
+          <img src='/icons/chevron-right.svg' alt='next' width='28' className='place-self-center'/>
+          </button>
         </div>
-        <div className='h-16 w-64 mr-4 ml-auto bg-white '>
-          <p className='mt-5 ml-5'>{userData.login}</p>
+
+        <div className='w-full h-12 pl-4 ml-4 mr-4 bg-white flex text-left
+          text-neutral-800
+          bg-gradient-to-b from-neutral-300 to-neutral-400 
+          border-solid border-2 border-neutral-400 rounded-lg'>
+            <p className='place-self-center'>Route</p>
         </div>
-        <button className='w-16 
-        bg-white'
-        onClick={logOut}>
-          Logout
-        </button>
+
+        <div className='ml-auto flex'>
+          <div className='w-60 h-12 pl-4 flex text-left
+          bg-gradient-to-b from-neutral-700 to-neutral-800 border-neutral-800         
+          border-solid border-2 rounded-l-md outline-none'>
+            <p className='place-self-center'>{userData.login}</p>
+          </div>
+          <button className='w-12 h-12 grid
+          bg-gradient-to-b from-neutral-700 to-neutral-800 border-neutral-800 
+          hover:from-neutral-600 hover:to-neutral-700 hover:border-neutral-700           
+          border-solid border-2 rounded-r-md outline-none'
+          onClick={logOut}>
+            <img src='/icons/box-arrow-right.svg' alt='prev' width='28' className='place-self-center'/>
+          </button >    
+        </div>
+        
     </div>
   );
 }
