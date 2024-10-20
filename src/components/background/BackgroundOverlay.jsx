c602cc9cecf876513cc91c0048e9a2ee8e7e1a4c
Igor <54187585+StingrayFG@users.noreply.github.com>
2024-10-20 21:13:52 +0300

    Update bg overlay z-index

--- src/components/background/BackgroundOverlay.jsx
+++ src/components/background/BackgroundOverlay.jsx
@@ -4,13 +4,13 @@ import { Box } from '@mui/material';
 export default function BackgroundOverlay() {
   return (
     <>
-      <Box className='w-full h-full z-[-50]
+      <Box className='w-full h-full z-[-40]
       fixed bg-neutral-950/80' />
 
-      <Box className='w-full h-full z-[-50]
+      <Box className='w-full h-full z-[-40]
       fixed bg-sky-950/10' />
 
-      <Box className='w-screen h-dvh z-[-50]
+      <Box className='w-screen h-dvh z-[-40]
       fixed overflow-hidden pointer-events-none noise opacity-40' />
     </>
   )
