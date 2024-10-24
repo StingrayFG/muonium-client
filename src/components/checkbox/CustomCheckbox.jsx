e75dba61118d5e2280ef436a87bfc7a7b03455d3
Igor <54187585+StingrayFG@users.noreply.github.com>
2024-10-25 02:25:04 +0300

    Update CustomCheckbox
    Update CustomCheckbox to not use MUI checkbox component, add context menu mode (makes the checkbox smaller, allows the parent option to switch the checkbox, disable in-component state switch on click)

--- src/components/checkbox/CustomCheckbox.jsx
+++ src/components/checkbox/CustomCheckbox.jsx
@@ -1,33 +1,40 @@
 import { Box, Checkbox } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { ReactComponent as CheckLg } from 'assets/icons/check-lg.svg'
 
 
-export default function CustomCheckbox ({ defaultValue, setMenuValue }) {
+export default function CustomCheckbox ({ defaultValue, setMenuValue, isContextMenuMode }) {
 
   const [value, setValue] = useState(defaultValue ? defaultValue : false);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-    setMenuValue(newValue);
+  useEffect(() => {
+    if (defaultValue !== value) {
+      setValue(defaultValue ? defaultValue : false);
+    }
+  }, [defaultValue]);
+
+  const handleOnClick = () => {
+    if (!isContextMenuMode) {
+      setValue(!value);
+      setMenuValue(!value);
+    }
   }
 
   return (
-    <Box className='h-8 w-8 relative 
-    grid place-content-center place-items-center'>
-      <Checkbox className='opacity-0'
-      onChange={handleChange}
-      checked={value}
-      />
+    <Box className={`h-8 relative cursor-default
+    grid place-content-center place-items-center
+    ${isContextMenuMode ? 'w-4' : 'w-8'}`}>
 
-      <Box className={`h-6 w-6 absolute pointer-events-none
-      grid place-items-center
+      <Box className={`absolute grid
       transition-all duration-300
       border rounded-[0.3rem] 
-      ${value ? 'border-sky-400/40 bg-sky-400/20' : 'border-neutral-200/20 bg-neutral-200/10'}`}>
-        <CheckLg className={`h-5 w-5
+      ${isContextMenuMode ? 'h-4 w-4' : 'h-6 w-6'}
+      ${value ? 'border-sky-400/40 bg-sky-400/20' : 'border-neutral-200/20 bg-neutral-200/10'}`}
+      onClick={handleOnClick}>
+        <CheckLg className={`place-self-center
         transition-all duration-300
+        ${isContextMenuMode ? 'h-3 w-3' : 'h-5 w-5'}
         ${value ? 'opacity-100' : 'opacity-0'}`} />
       </Box>
 
