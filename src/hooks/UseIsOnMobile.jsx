1f6a88ebfde15fd2aeee24f9c8bd5342ac6820ec
StingrayFG <54187585+StingrayFG@users.noreply.github.com>
2025-07-29 22:11:09 +0000

    Update the UseIsOnMobile custom hook

--- src/hooks/UseIsOnMobile.jsx
+++ src/hooks/UseIsOnMobile.jsx
@@ -1,18 +1,5 @@
-import { useEffect, useState } from 'react';
 
 
 export function useIsOnMobile () {
-  const [isOnMobile, setIsOnMobile] = useState(true);
-  
-  const setState = () => {
-    setIsOnMobile(window.innerWidth < 768);
-  }
-
-  useEffect(() => {
-    setState();
-    window.addEventListener('resize', setState);
-    return () => window.removeEventListener('resize', setState);
-  }, [])
-
-  return isOnMobile;
+  return window.innerWidth < 768;
 }
